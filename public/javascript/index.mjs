48a563419bdeebc6a2c28ae309ccd9edb2bcb6aa
Logyre <logarithmicpresence@gmail.com>
2024-07-17 01:09:27 +0800

    Succesfully added new figma files to database

--- public/javascript/index.mjs
+++ public/javascript/index.mjs
@@ -3,3 +3,21 @@
  * Nav bar functionality
  */
 
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {applySticky()};
+
+// Get the offset position of the navbar
+var navbar = document.getElementById("navbar");
+console.log(navbar);
+var sticky = navbar?.offsetTop;
+
+function applySticky() {
+
+    if (window.scrollY >= sticky) {
+        navbar.classList.add("sticky")
+        console.log(sticky);
+    } else {
+        navbar.classList.remove("sticky");
+    }
+  
+}
