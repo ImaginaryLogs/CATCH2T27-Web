570236509be8319e2fd51dcbd62b3ae7b182171d
sean-benedict-bernardo <145823446+sean-benedict-bernardo@users.noreply.github.com>
2024-08-31 15:01:32 +0800

    Updated tailwind.config.js to contain colors as per CATCH2T27 brand book.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,svelte,js,ts}'],
+  // brand book reference
+  // https://www.canva.com/design/DAF5Ty7YwNw/QmerhPgZ7F0L3jE3NuXPLg/edit
   theme: {
+    colors: {
+      "cream": "#FDE9DF",
+      "light-purple": "#E3BCD8",
+      "purple": "#743E79",
+      "dark-purple": "#200D32",
+      "black": "#0F1315",
+    },
     extend: {},
   },
   plugins: [],
