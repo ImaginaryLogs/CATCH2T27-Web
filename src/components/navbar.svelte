bc636ca593202cc1ea0ca0f2f8f471c1a3c93bd6
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2024-09-01 14:40:48 +0800

    added x-padding to navbar

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,44 +1,60 @@
 <!--Navigation bar for all the webpages-->
 
-<div id="nav-bar" class="flex flex-auto flex-row py-2">
-    <div id="catchlogo" class="flex flex-auto justify-start">
-        <a href="/" id="home">
-            <img src="/images/colored-20.png" alt="catch2t27img" class="size-16"/>
-        </a>
-    </div>
+<div id="nav-bar" class="flex flex-auto flex-row py-2 px-6">
+  <div id="catchlogo" class="flex flex-auto justify-start">
+    <a href="/" id="home">
+      <img src="/images/colored-20.png" alt="catch2t27img" class="size-16" />
+    </a>
+  </div>
 
-    <div id="data-component" class="flex flex-auto flex-row justify-end space-x-10">
-        <a href="/pertinentlinks" id="pertinentlinks">
-        <div id="pertinentlinks-component" class="flex flex-auto flex-col items-center">
-            <p class="font-head">News</p>
-            <img src="/images/nav/icon-newspaper0.svg" alt="infoimg" class="size-8"/>
-        </div>
-
-
+  <div
+    id="data-component"
+    class="flex flex-auto flex-row justify-end space-x-10"
+  >
+    <a href="/pertinentlinks" id="pertinentlinks">
+      <div
+        id="pertinentlinks-component"
+        class="flex flex-auto flex-col items-center"
+      >
+        <p class="font-head">News</p>
+        <img
+          src="/images/nav/icon-newspaper0.svg"
+          alt="infoimg"
+          class="size-8"
+        />
+      </div>
     </a>
 
-        <a href="/archivespage" id="archivespage">
-            <div id="archivespage-component" class="flex flex-auto flex-col items-center">
-                <p class="font-head">Archives</p>
-                <img src="/images/nav/icon-boxes0.svg" alt="archivesimg" class="size-8"
-                />
-            </div>
+    <a href="/archivespage" id="archivespage">
+      <div
+        id="archivespage-component"
+        class="flex flex-auto flex-col items-center"
+      >
+        <p class="font-head">Archives</p>
+        <img
+          src="/images/nav/icon-boxes0.svg"
+          alt="archivesimg"
+          class="size-8"
+        />
+      </div>
+    </a>
 
-        </a>
+    <a href="/calendar" id="calendar">
+      <div id="calendar-component" class="flex flex-col items-center">
+        <p class="font-head">Calendar</p>
+        <img
+          src="/images/nav/icon-calendar0.svg"
+          alt="calendarimg"
+          class="size-8"
+        />
+      </div>
+    </a>
 
-        <a href="/calendar" id="calendar">
-            <div id="calendar-component" class="flex flex-col items-center">
-                <p class="font-head">Calendar</p>
-                <img src="/images/nav/icon-calendar0.svg" alt="calendarimg" class="size-8"/>
-            </div>
-        </a>
-
-        
-        <a href="/aboutus" id="aboutus">
-            <div id="aboutus-component" class="flex flex-col items-center">
-                <p class="font-head">About Us</p>
-                <img src="/images/nav/icon-info0.svg" alt="aboutusimg" class="size-8"/>
-            </div>
-        </a>
-    </div>
+    <a href="/aboutus" id="aboutus">
+      <div id="aboutus-component" class="flex flex-col items-center">
+        <p class="font-head">About Us</p>
+        <img src="/images/nav/icon-info0.svg" alt="aboutusimg" class="size-8" />
+      </div>
+    </a>
+  </div>
 </div>
