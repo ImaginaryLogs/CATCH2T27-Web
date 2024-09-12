98309e33a4324835fe684b1ef50354b57f4892ef
Diego Yason <61825575+diego-yason@users.noreply.github.com>
2024-09-12 10:32:59 +0800

    started on adding a backend connection to firebase

---
+++ src/lib/server/supabase.ts
@@ -0,0 +1,3 @@
+import { initializeApp } from "firebase-admin/app"; // TODO: install firebase-admin to repo
+
+export const app = initializeApp({ credential: applicationDefault() databaseURL: "TODO!!!!!!!!!!!!!!"});
