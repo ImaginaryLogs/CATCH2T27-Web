613ae740db19272088d384798ac3e2b6fd120cae
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2024-09-13 17:35:41 +0800

    integrated firebase to the project

--- src/lib/server/supabase.ts
+++ src/lib/server/supabase.ts
@@ -1,3 +1,12 @@
-import { initializeApp } from "firebase-admin/app"; // TODO: install firebase-admin to repo
+// import { initializeApp } from "firebase-admin/app"; // TODO: install firebase-admin to repo
+import admin from "firebase-admin";
 
-export const app = initializeApp({ credential: applicationDefault() databaseURL: "TODO!!!!!!!!!!!!!!"});
+import { GOOGLE_APPLICATION_CREDENTIALS } from "$env/static/private";
+
+export const app = admin.initializeApp({
+  credential: admin.credential.cert(
+    await import(GOOGLE_APPLICATION_CREDENTIALS)
+  ),
+});
+
+console.log("Firebase app initialized");
