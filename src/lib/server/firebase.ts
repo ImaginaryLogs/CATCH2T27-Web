88d76c0f4579f7fad6bd164424895372bf887faf
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2024-09-13 18:00:52 +0800

    updated implementation of server credentials

--- src/lib/server/firebase.ts
+++ src/lib/server/firebase.ts
@@ -1,11 +1,16 @@
 import admin from "firebase-admin";
 
-import { GOOGLE_APPLICATION_CREDENTIALS } from "$env/static/private";
+import { GOOGLE_APPLICATION_KEY } from "$env/static/private";
+import credentials from "./credentials.json";
 
-export const app = admin.initializeApp({
-  credential: admin.credential.cert(
-    await import(GOOGLE_APPLICATION_CREDENTIALS)
-  ),
-});
+export const app =
+  admin.apps.length == 0
+    ? admin.initializeApp({
+        credential: admin.credential.cert({
+          privateKey: GOOGLE_APPLICATION_KEY,
+          ...credentials,
+        }),
+      })
+    : admin.app();
 
 console.log("Firebase app initialized");
