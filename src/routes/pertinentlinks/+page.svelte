90b1529b62ce8093584cd57ad67cfdd0a2ee1928
Logyre <logarithmicpresence@gmail.com>
2024-09-22 22:43:07 +0800

    Small pertinent link heading readjustment
    As per Sean's suggestion

--- src/routes/pertinentlinks/+page.svelte
+++ src/routes/pertinentlinks/+page.svelte
@@ -9,8 +9,8 @@
 
 </script>
 <div id="links-background-component" class="flex flex-col bg-dark-purple w-full h-full min-h-screen">
-    <p class="text-white text-lg font-head self-center mt-4">Pertinent Links</p>
-    <p class="text-white font-subhead self-center">Updated as of {date_updated}</p>
+    <p class="text-white text-4xl font-head self-center mt-4">Pertinent Links</p>
+    <p class="text-white font-xl self-center">Updated as of {date_updated}</p>
     <br>
     <div id="links-body-component" class="flex self-center bg-white rounded-3xl px-60 p-5">
         <LinksList/>
