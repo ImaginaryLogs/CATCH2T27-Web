b26d83e4bd633c9648d1b9acc9352dd83fd93f35
IWillRekU3206 <37181282+iwillreku3206@users.noreply.github.com>
2024-09-26 20:04:51 +0800

    ga image (maybe0

--- src/routes/aboutus/GeneralAssembly.svelte
+++ src/routes/aboutus/GeneralAssembly.svelte
@@ -1,7 +1,7 @@
 <div class="m-5 flex flex-col items-center justify-center gap-2 min-h-screen">
   <h2 class="font-body font-bold text-4xl">CATCH2T27 General Assembly T1</h2>
   <img
-    src="https://placehold.co/1920x1080"
+    src="/static/images/archives/GA1_main.jpg"
     alt="Members of CATCH2T27"
     class="aspect-auto w-[60vw]"
   />
